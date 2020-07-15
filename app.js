81a3ef69f59a698ccc9035c15d9d6e80e8fc0b2a
hebrewmatthew <46854918+hebrewmatthew@users.noreply.github.com>
2020-07-15 08:55:49 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,47 @@
+showHealingMatt12() {
+
+healingVersesMatt12 = "<div class="grid-container"><div class="grid-item">and behold, a man whose hand was withered, and they asked him, saying, “is it permissible to heal the sick on Shabbat?” And this entire fabric[ated question] was simply [designed to give them a reason] to snitch on him to the Courthouse.</div>
+  <div class="grid-item">10 </div>
+  <div class="grid-item">והנה אדם שיבשה ידו וישאלו לו לאמר אם מותר בשבת לרפא לחולים וכל זה בד להלשינו לבית דין </div></div>
+<br><br>
+ <div class="grid-container"><div class="grid-item">Then he said to [the] man, “put forth your hand.” And he put [it] forth and it was restored to health like the other one. </div>
+<div class="grid-item">13 </div>
+  <div class="grid-item">אז אמר לאדם נטה את ידך ויט ותשב לרפאות כמו השנית</div></div>
+<br><br>
+<div class="grid-item">Then drew near one mastered by demons, [who was] blind and deaf, and he healed him so that he was able to speak. </div>
+<div class="grid-container"><div class="grid-item">22 </div>
+  <div class="grid-item">אז הגש אליו בעל השד עור וחרש וירפא אותו כדי שיכול לדבר</div></div>"; 
+
+document.getElementById("showspace").style.color = "white"; 
+
+document.getElementById("showspace").style.backgroundColor = "red";
+
+document.getElementById("showspace").innerHTML = healingVersesMatt12; 
+
+}
+
+showProphecyMatt12() {
+prophecyVersesMatt12 = "<div class="grid-container"><div class="grid-item">17 </div>
+  <div class="grid-item">שיתקיים מה שנאמר על ידי ישעיהו הנביא האומר הן עבדי אתמך בו בחורי רצתה נפשי נתתי רוחי עליו משפט לגוים יוציא</div>
+  <div class="grid-item">‘behold my servant. I will uphold him [who is] my chosen, the pleasure of my soul. I have put My Spirit upon him. He shall bring forth right-ruling to the nations. </div>
+  <div class="grid-item">18 </div>
+ <div class="grid-item">  הן עבדי אתמך בו בחורי רצתה נפשי נתתי רוחי עליו משפט לגוים יוציא
+ </div>
+   <div class="grid-item">He shall not cry out, and neither will he lift up [a shout], nor make his voice heard in the street. </div>
+<div class="grid-item">19 </div>
+  <div class="grid-item">לא יצעק ולא ישא ולא ישמיע בחוץ קולו </div>
+<div class="grid-item">He will not break a crushed reed, and will not extinguish a dim [wick of] flax until he place right-ruling in [the] land. </div>
+<div class="grid-item">20 </div>
+  <div class="grid-item">קנה רצוץ לא ישבור ופשתה כהה לא יכבנה לאמת יוציא משפט לא יכהה ולא ירוץ עד ישים בארץ משפט</div>
+ <div class="grid-item"> And the islands shall wait for his Torah.’ </div>
+ <div class="grid-item">21 </div>
+  <div class="grid-item">ולתורתו איים ייחלו</div></div>”;
+
+document.getElementById("showspace").style.color = "white"; 
+
+document.getElementById("showspace").style.backgroundColor = "deeppink";
+
+document.getElementById("showspace").innerHTML = prophecyVersesMatt12; 
+
+
+}
