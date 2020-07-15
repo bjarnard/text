23a08ea04570eec476374bf473e97b2ec9d2bcab
hebrewmatthew <46854918+hebrewmatthew@users.noreply.github.com>
2020-07-15 15:56:46 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+function showHealingMatt12() {
+
+  var healingMatt12 = '10 and behold, a man whose hand was withered, and they asked him, saying, “is it permissible to heal the sick on Shabbat?” And this entire fabric[ated question] was simply [designed to give them a reason] to snitch on him to the Courthouse.' + '<br><br>' +
+  '13 Then he said to [the] man, “put forth your hand.” And he put [it] forth and it was restored to health like the other one.' + '<br><br>' + '22 Then drew near one mastered by demons, [who was] blind and deaf, and he healed him so that he was able to speak.'; 
+
+document.getElementById("showspace").style.color = "white"; 
+document.getElementById("showspace").style.backgroundColor = "red";
+
+document.getElementById("showspace").innerHTML = healingMatt12;
+}
+
+function showProphecyMatt12() {
+
+  var prophecyMatt12 = '17 so that what was said at the hand of YeshiYahu the prophet would be established—the saying, <br><br> 18 ‘behold my servant. I will uphold him [who is] my chosen, the pleasure of my soul. I have put My Spirit upon him. He shall bring forth right-ruling to the nations.<br><br> 19 He shall not cry out, and neither will he lift up [a shout], nor make his voice heard in the street. <br><br> 20 He will not break a crushed reed, and will not extinguish a dim [wick of] flax until he place right-ruling in [the] land.<br><br>21 And the islands shall wait for his Torah.’ <br><br>';
+  document.getElementById("showspace").style.color = "white"; 
+  document.getElementById("showspace").style.backgroundColor = "deeppink";
+  
+  document.getElementById("showspace").innerHTML = prophecyMatt12;
+  }
+
+
+
+/* var contain = '<div class="grid-container">'
+var griditem = '<div class="grid-item">'
+
+function showHealingMatt12() {
+var verse = '10'; 
+var healingMatt12tenEn = 'and behold, a man whose hand was withered, and they asked him, saying, “is it permissible to heal the sick on Shabbat?” And this entire fabric[ated question] was simply [designed to give them a reason] to snitch on him to the Courthouse.'; 
+var healingMatt12tenHe = 'והנה אדם שיבשה ידו וישאלו לו ל אמר אם מותר בשבת לרפא לחולים וכל זה בד להלשינו לבית דין'
+
+document.getElementById("showspaceEn").style.color = "white"; 
+
+document.getElementById("showspaceEn").style.backgroundColor = "red";
+document.getElementById("showspaceHe").style.backgroundColor = "red";
+
+document.getElementById("showspaceEn").innerHTML = healingMatt12tenEn; 
+document.getElementById("showspaceHe").innerHTML = healingMatt12tenHe; 
+document.getElementById("verse").innerHTML = verse; 
+
+}
+/*
+function showProphecyMatt12() {
+var prophecyVersesMatt12 = '<div class="grid-container"><div class="grid-item">17 </div>
+  <div class="grid-item">שיתקיים מה שנאמר על ידי ישעיהו הנביא האומר הן עבדי אתמך בו בחורי רצתה נפשי נתתי רוחי עליו משפט לגוים יוציא</div>
+  <div class="grid-item">‘behold my servant. I will uphold him [who is] my chosen, the pleasure of my soul. I have put My Spirit upon him. He shall bring forth right-ruling to the nations. </div>
+  <div class="grid-item">18 </div>
+ <div class="grid-item">  הן עבדי אתמך בו בחורי רצתה נפשי נתתי רוחי עליו משפט לגוים יוציא
+ </div>
+   <div class="grid-item">He shall not cry out, and neither will he lift up [a shout], nor make his voice heard in the street. </div>
+<div class="grid-item">19 </div>
+  <div class="grid-item">לא יצעק ולא ישא ולא ישמיע בחוץ קולו </div>
+<div class="grid-item">He will not break a crushed reed, and will not extinguish a dim [wick of] flax until he place right-ruling in [the] land. </div>
+<div class="grid-item">20 </div>
+  <div class="grid-item">קנה רצוץ לא ישבור ופשתה כהה לא יכבנה לאמת יוציא משפט לא יכהה ולא ירוץ עד ישים בארץ משפט</div>
+ <div class="grid-item"> And the islands shall wait for his Torah.’ </div>
+ <div class="grid-item">21 </div>
+  <div class="grid-item">ולתורתו איים ייחלו</div></div>';
+
+document.getElementById("showspace").style.color = "white"; 
+
+document.getElementById("showspace").style.backgroundColor = "deeppink";
+
+document.getElementById("showspace").innerHTML = prophecyVersesMatt12; 
+
+
+}
+*/
